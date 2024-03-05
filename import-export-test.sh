601954e55fa1312dca2393aeda64ae7ad7c3e3a1
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-03-05 15:25:18 -0800

    Update import-export-test.sh

--- import-export-test.sh
+++ import-export-test.sh
@@ -1,9 +1,4 @@
+export CPDCTL_ENABLE_DSJOB=true
 cpdctl dsjob export-zip --project DataStagePIPELINE --name TestFlowA --file-name test-export-zip 
-
-wait 50
-
 cpdctl dsjob import-zip --project DataStagePIPELINE --file-name test-export-zip 
-
-wait 50
-
 cpdctl dsjob get-import-zip --project DataStagePIPELINE --import-id 24576d5e-dac3-420a-992f-26db66bb81ca
