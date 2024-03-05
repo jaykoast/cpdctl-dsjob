c702e0619008f7cdb750c63862e81e7cdc9ac24b
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-03-05 13:37:08 -0800

    Update cpdctl-install.sh

--- cpdctl-install.sh
+++ cpdctl-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://github.com/IBM/cpdctl/releases/download/v1.4.116/cpdctl_darwin_amd64.tar.gz 
 tar zxf cpdctl_darwin_amd64.tar.gz
-chmo 777 cpdctl
+chmod 777 cpdctl
 sudo cp cpdctl /bin
