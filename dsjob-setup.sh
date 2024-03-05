85f92d4db2cfcd1d9ff42497f39b85aa424a4ba1
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-03-05 14:46:57 -0800

    Update dsjob-setup.sh

--- dsjob-setup.sh
+++ dsjob-setup.sh
@@ -3,6 +3,7 @@ export DSJOB_URL=https://cpd-cpd-instance.apps.65c407d60139de0011c3d408.cloud.te
 export DSJOB_USER=cpadmin
 export DSJOB_PWD=0ZMpI5aAq562zzmCrK5PiqsBOf4KEOXL
 export CPDCTL_ENABLE_DSJOB=true
+echo $PATH
 cpdctl config user set CP4D-user --username $DSJOB_USER --password $DSJOB_PWD
 cpdctl config profile set CP4D-profile --url $DSJOB_URL --user CP4D-user
 cpdctl config profile use CP4D-profile
