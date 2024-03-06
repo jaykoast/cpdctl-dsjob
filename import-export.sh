3277121f55a86084f30e5a42f6017dcc28c6bfe4
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-03-05 16:32:14 -0800

    Update import-export.sh

--- import-export.sh
+++ import-export.sh
@@ -1,5 +1,5 @@
 export CPDCTL_ENABLE_DSJOB=true
-cpdctl config profile set cpd --url --username cpadmin --apikey RRHgqmAl7aAv8xjFf6vKsPBfoSWWu6HtaOGcOM2M
+cpdctl config profile set cpd --url https://cpd-cpd-instance.apps.65c407d60139de0011c3d408.cloud.techzone.ibm.com --username cpadmin --apikey RRHgqmAl7aAv8xjFf6vKsPBfoSWWu6HtaOGcOM2M
 cpdctl config profile use cpd
 cpdctl dsjob export-zip --project DataStagePIPELINE --name TestFlowA --file-name test-export-zip 
 cpdctl dsjob import-zip --project DataStagePIPELINE --file-name test-export-zip 
