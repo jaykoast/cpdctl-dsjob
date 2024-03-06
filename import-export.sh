102b1773ea70fbcb2a9781830ebd3309a6d51044
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-03-05 16:11:44 -0800

    Update import-export.sh

--- import-export.sh
+++ import-export.sh
@@ -1,4 +1,6 @@
 export CPDCTL_ENABLE_DSJOB=true
+cpdctl config profile set cpdaas --url https://cloud.ibm.com --apikey RRHgqmAl7aAv8xjFf6vKsPBfoSWWu6HtaOGcOM2M
+cpdctl config profile use cpdaas
 cpdctl dsjob export-zip --project DataStagePIPELINE --name TestFlowA --file-name test-export-zip 
 cpdctl dsjob import-zip --project DataStagePIPELINE --file-name test-export-zip 
 cpdctl dsjob get-import-zip --project DataStagePIPELINE --import-id 24576d5e-dac3-420a-992f-26db66bb81ca
