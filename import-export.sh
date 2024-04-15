85b77cf89d167d1d0e849d9424012960cd3bc551
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-04-15 08:32:51 -0700

    updated import stuff

--- import-export.sh
+++ import-export.sh
@@ -2,6 +2,8 @@ export DSJOB_URL=https://cpd-cpd.apps.ocp-12000076xq-5wvu.cloud.techzone.ibm.com
 export DSJOB_USER=kubeadmin
 export DSJOB_PWD=XT5MI-w6T69-BP2j3-vbTtP
 export CPDCTL_ENABLE_DSJOB=true
+cpdctl config user set CP4D-user --username $DSJOB_USER --password $DSJOB_PWD
+cpdctl config profile set CP4D-profile --url $DSJOB_URL --user CP4D-user
 cpdctl config profile use CP4D-profile
 cpdctl dsjob export --project DataGovProject --name ds1 --file-name test-export-flow
 sleep 10
