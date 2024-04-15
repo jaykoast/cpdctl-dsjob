fb24eae5d216dc973b9798ac035be6419f6fb7d0
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-04-15 07:09:30 -0700

    Update dsjob-setup.sh with export

--- dsjob-setup.sh
+++ dsjob-setup.sh
@@ -6,8 +6,8 @@ export CPDCTL_ENABLE_DSJOB=true
 cpdctl config user set CP4D-user --username $DSJOB_USER --password $DSJOB_PWD
 cpdctl config profile set CP4D-profile --url $DSJOB_URL --user CP4D-user
 cpdctl config profile use CP4D-profile
-export CPDCTL_ENABLE_DSJOB=true
-cpdctl dsjob export-zip --project DataGovProject --name ds1 --file-name ./TESING-EXPORT 
+cpdctl dsjob export-zip --project DataGovProject --name ds1 --file-name TESING-EXPORT 
 sleep 10
+export
 cpdctl dsjob import-zip --project TestingPipeline --file-name ./TESTING-EXPORT
 sleep 10
