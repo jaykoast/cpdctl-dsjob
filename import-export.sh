d9d1a098d826c4a08b77792213e4c1a5ea7f8a0a
Johans M Acosta <63568771+jaykoast@users.noreply.github.com>
2024-04-15 08:24:08 -0700

    updated import & export zip

--- import-export.sh
+++ import-export.sh
@@ -1,6 +1,8 @@
 export CPDCTL_ENABLE_DSJOB=true
-cpdctl dsjob export-zip --project DataGovProject --name ds1 --file-name TESING-EXPORT 
+cpdctl config profile use CP4D-profile
+cpdctl dsjob export --project DataGovProject --name ds1 --file-name test-export-flow
 sleep 10
-cpdctl dsjob import-zip --project TestingPipeline --file-name TESTING-EXPORT
+cpdctl dsjob save-export --project DataGovProject --name ds1 --file-name test-export-flow
 sleep 10
+cpdctl dsjob list-exports --project DataGovProject
 # cpdctl dsjob get-import-zip --project DataStagePIPELINE --import-id 24576d5e-dac3-420a-992f-26db66bb81ca
